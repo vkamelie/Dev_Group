42333e539b3ba1287d90bfcbc645857b61684a75
= <=>
2019-05-20 15:49:14 -0700

    added post and get for school and reviews

--- server/index.js
+++ server/index.js
@@ -15,8 +15,8 @@ app.use(express.json());
 massive(CONNECTION_STRING)
   .then(db => {
     console.log("connected to db");
-      app.set("db", db);
-      db.init()
+    app.set("db", db);
+    db.init();
   })
   .catch(err => {
     console.log("Failed to connect to db");
@@ -36,12 +36,24 @@ app.get("/schools/:id/reviews", async (req, res) => {
   const db = req.app.get("db");
   const school = data.filter(school => school.id == id);
   const reviews = await db.get_reviews(id);
+  //send all the school info PLUS all reviews associated with them
   res.status(200).send({
     school,
     reviews
   });
 });
 
+app.post("/schools/:id/reviews", async (req, res, next) => {
+  //We will probably need to add LOGGED IN middleware here
+  const { course, rating, post } = req.body; //We will need to talk about the data being sent up from the client, this is a rough draft
+  //I imagine the user will fill in the course they took?
+  const user_id = req.session.user.id; //This is assuming we are using sessions. This is the user id
+  const { id } = req.params; //This is the school id
+  const db = req.app.get("db");
+  //Just send the updated reviews
+  const newReviews = await db.add_review([user_id, id, course, rating, post]);
+  res.status(200).send(newReviews);
+});
 
 app.listen(PORT, () => {
   console.log(`Server has started on port ${PORT}`);
