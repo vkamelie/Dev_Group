b68dd337867ff8206f9d3fa3cf06577382a006f9
= <=>
2019-05-21 10:24:24 -0700

    set up endpoints for searching for schools by name

--- server/index.js
+++ server/index.js
@@ -1,4 +1,5 @@
 const { data } = require("./schools");
+
 require("dotenv").config();
 //Environment Variables
 const { PORT, CONNECTION_STRING } = process.env;
@@ -7,16 +8,25 @@ const { PORT, CONNECTION_STRING } = process.env;
 const express = require("express");
 const app = express();
 const massive = require("massive");
+//const session = require("express-session");
 
-//Top level middlewares
-app.use(express.json());
+// //Top level middlewares
+// app.use(express.json());
+// app.use(
+//   session({
+//     secret: "keyboard cat",
+//     resave: false,
+//     saveUninitialized: true,
+//     cookie: { secure: true }
+//   })
+// );
 
 //Database
 massive(CONNECTION_STRING)
   .then(db => {
     console.log("connected to db");
     app.set("db", db);
-    db.init();
+    db.init(); //Make sure we take this out when we run our build
   })
   .catch(err => {
     console.log("Failed to connect to db");
@@ -24,36 +34,17 @@ massive(CONNECTION_STRING)
 
 //Middlewares
 const reviewsController = require("./Controllers/reviewsController");
+const schoolsController = require("./Controllers/schoolsControllers");
 
 //Endpoints
-app.get("/schools/all", (req, res) => {
-  res.status(200).send(data); //For getting all data yooo
-});
 
-app.get("/schools/:id/reviews", async (req, res) => {
-  //We want the information about the school, as well as all of the reviews that go with the school
-  const { id } = req.params;
-  const db = req.app.get("db");
-  const school = data.filter(school => school.id == id);
-  const reviews = await db.get_reviews(id);
-  //send all the school info PLUS all reviews associated with them
-  res.status(200).send({
-    school,
-    reviews
-  });
-});
+//This returns all info for a school, which includes the reviews associated with it
+app.get("/schools/:id/reviews", reviewsController.getReviews);
+app.post("/schools/:id/reviews", reviewsController.addReview);
 
-app.post("/schools/:id/reviews", async (req, res, next) => {
-  //We will probably need to add LOGGED IN middleware here
-  const { course, rating, post } = req.body; //We will need to talk about the data being sent up from the client, this is a rough draft
-  //I imagine the user will fill in the course they took?
-  const user_id = req.session.user.id; //This is assuming we are using sessions. This is the user id
-  const { id } = req.params; //This is the school id
-  const db = req.app.get("db");
-  //Just send the updated reviews
-  const newReviews = await db.add_review([user_id, id, course, rating, post]);
-  res.status(200).send(newReviews);
-});
+//This is what is fired from the input in Home.js. This is used to search for school by keyword, which opens a modal
+app.get("/schools", schoolsController.getSchoolQuery);
+app.get("/schools/all", schoolsController.getAllSchools);
 
 app.listen(PORT, () => {
   console.log(`Server has started on port ${PORT}`);
